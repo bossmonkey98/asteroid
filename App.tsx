d081defcd11a893ed91b38ecfd260dfe45457bfd
bossmonkey98 <47918140+bossmonkey98@users.noreply.github.com>
2022-10-08 21:50:15 +0530

    Screen Routes Defined

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Asteroid from './Screens/Asteroid';
+import Home from './Screens/Home';
+import { s } from 'react-native-wind'
+
+export type ScreenParamList = {
+  Home: undefined;
+  Asteroid: { AsteroidId: string | number | null | undefined }
+}
 
 export default function App() {
+  const url: string = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY`
+  const Stack = createNativeStackNavigator<ScreenParamList>()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <View style={s`h-full`}>
+        <Stack.Navigator initialRouteName='Home'>
+          <Stack.Screen name='Home' component={Home} options={{ title: "Home" }} />
+          <Stack.Screen name='Asteroid' component={Asteroid} options={{ title: "About Asteroid" }} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
